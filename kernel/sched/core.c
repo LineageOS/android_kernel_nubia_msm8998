8482d334fa19798dd104a979113f943fabca39e5
Peter Zijlstra <peterz@infradead.org>
2016-09-20 22:06:01 +0200

    sched/core: Fix set_user_nice()
    Almost all scheduler functions update state with the following
    pattern:
    
    	if (queued)
    		dequeue_task(rq, p, DEQUEUE_SAVE);
    	if (running)
    		put_prev_task(rq, p);
    
    	/* update state */
    
    	if (queued)
    		enqueue_task(rq, p, ENQUEUE_RESTORE);
    	if (running)
    		set_curr_task(rq, p);
    
    set_user_nice() however misses the running part, cure this.
    
    This was found by asserting we never enqueue 'current'.
    
    Change-Id: I16faa75d3e3100b880f04610c1ae355871b4cdda
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Cc: Linus Torvalds <torvalds@linux-foundation.org>
    Cc: Mike Galbraith <efault@gmx.de>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Cc: linux-kernel@vger.kernel.org
    Signed-off-by: Ingo Molnar <mingo@kernel.org>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -3995,7 +3995,8 @@ out_unlock:
 
 void set_user_nice(struct task_struct *p, long nice)
 {
-	int old_prio, delta, queued;
+	bool queued, running;
+	int old_prio, delta;
 	unsigned long flags;
 	struct rq *rq;
 
@@ -4019,8 +4020,11 @@ void set_user_nice(struct task_struct *p, long nice)
 		goto out_unlock;
 	}
 	queued = task_on_rq_queued(p);
+	running = task_current(rq, p);
 	if (queued)
 		dequeue_task(rq, p, DEQUEUE_SAVE);
+	if (running)
+		put_prev_task(rq, p);
 
 	p->static_prio = NICE_TO_PRIO(nice);
 	set_load_weight(p);
@@ -4037,6 +4041,8 @@ void set_user_nice(struct task_struct *p, long nice)
 		if (delta < 0 || (delta > 0 && task_running(rq, p)))
 			resched_curr(rq);
 	}
+	if (running)
+		p->sched_class->set_curr_task(rq);
 out_unlock:
 	task_rq_unlock(rq, p, &flags);
 }
