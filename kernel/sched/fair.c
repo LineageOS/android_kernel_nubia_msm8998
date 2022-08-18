44ed89a0c3ce747bc3ad78810f573a5a082e78c2
Chengming Zhou <zhouchengming@bytedance.com>
2022-08-18 20:47:59 +0800

    sched/fair: Reset sched_avg last_update_time before set_task_rq()
    set_task_rq() -> set_task_rq_fair() will try to synchronize the blocked
    task's sched_avg when migrate, which is not needed for already detached
    task.
    
    task_change_group_fair() will detached the task sched_avg from prev cfs_rq
    first, so reset sched_avg last_update_time before set_task_rq() to avoid that.
    
    [beykerykt]: Adapt for qcom_msm8998
    Change-Id: I4a85e6e3bae0ecfcb1ddd1baeed3961c7b98e05c
    Signed-off-by: Chengming Zhou <zhouchengming@bytedance.com>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Reviewed-by: Dietmar Eggemann <dietmar.eggemann@arm.com>
    Reviewed-by: Vincent Guittot <vincent.guittot@linaro.org>
    Link: https://lore.kernel.org/r/20220818124805.601-4-zhouchengming@bytedance.com

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -12437,12 +12437,12 @@ static void task_set_group_fair(struct task_struct *p)
 static void task_move_group_fair(struct task_struct *p)
 {
 	detach_task_cfs_rq(p);
-	set_task_rq(p, task_cpu(p));
 
 #ifdef CONFIG_SMP
 	/* Tell se's cfs_rq has been changed -- migrated */
 	p->se.avg.last_update_time = 0;
 #endif
+	set_task_rq(p, task_cpu(p));
 	attach_task_cfs_rq(p);
 }
 
