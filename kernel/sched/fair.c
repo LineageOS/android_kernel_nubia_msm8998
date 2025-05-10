84fed4a843bfe1be4eec985c775b2b57cfe2a1d4
BeYkeRYkt <beykerykt@gmail.com>
2025-05-11 00:16:29 +0900

    Revert "sched: Fix SCHED_HRTICK bug leading to late preemption of tasks"
    This reverts commit 82f6fb4f5c85f3842a65b15142ecd8378893dac4.
    
    Reason: Will be reapplied with the upstream commit
    
    Change-Id: If6080a59c0b3f9bf812fb08dee995b5e9e7571e2

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -5876,7 +5876,7 @@ static void hrtick_start_fair(struct rq *rq, struct task_struct *p)
 
 	WARN_ON(task_rq(p) != rq);
 
-	if (rq->cfs.h_nr_running > 1) {
+	if (cfs_rq->nr_running > 1) {
 		u64 slice = sched_slice(cfs_rq, se);
 		u64 ran = se->sum_exec_runtime - se->prev_sum_exec_runtime;
 		s64 delta = slice - ran;
@@ -5892,7 +5892,8 @@ static void hrtick_start_fair(struct rq *rq, struct task_struct *p)
 
 /*
  * called from enqueue/dequeue and updates the hrtick when the
- * current task is from our class.
+ * current task is from our class and nr_running is low enough
+ * to matter.
  */
 static void hrtick_update(struct rq *rq)
 {
@@ -5901,7 +5902,8 @@ static void hrtick_update(struct rq *rq)
 	if (!hrtick_enabled(rq) || curr->sched_class != &fair_sched_class)
 		return;
 
-	hrtick_start_fair(rq, curr);
+	if (cfs_rq_of(&curr->se)->nr_running < sched_nr_latency)
+		hrtick_start_fair(rq, curr);
 }
 #else /* !CONFIG_SCHED_HRTICK */
 static inline void
