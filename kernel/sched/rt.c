f4ef842a50b090ee357618c1a67193aa18b27425
Vladimir Mikhailov <beykerykt@gmail.com>
2025-05-28 01:26:49 +0900

    sched: rt: hmp: Do not place RT tasks on cores running softirqs
     * This commit applies the following commits to HMP:
       - c3544e3 (“sched: avoid scheduling RT threads on cores currently handling softirqs”)
       - 25e8ecf (“sched: avoid migrating when softint on tgt cpu should be short”)
       - c25c61b ("ANDROID: sched/rt: rt cpu selection integration with EAS")
       - 97eca2d (“softirq, sched: reduce softirq conflicts with RT”)
       Update RT HMP code to match our upstream.
    
    Change-Id: If1d5d6804c107c72c93341a5c22eb2f4142c87a1

--- kernel/sched/rt.c
+++ kernel/sched/rt.c
@@ -1595,22 +1595,6 @@ cpu_has_rt_task(int cpu)
 #ifdef CONFIG_SMP
 static int find_lowest_rq(struct task_struct *task, int sync);
 
-#ifdef CONFIG_SCHED_HMP
-static int
-select_task_rq_rt_hmp(struct task_struct *p, int cpu, int sd_flag, int flags)
-{
-	int target;
-
-	rcu_read_lock();
-	target = find_lowest_rq(p, 0);
-	if (target != -1)
-		cpu = target;
-	rcu_read_unlock();
-
-	return cpu;
-}
-#endif
-
 /*
  * Return whether the task on the given cpu is currently non-preemptible
  * while handling a potentially long softint, or if the task is likely
@@ -1657,6 +1641,60 @@ static void schedtune_dequeue_rt(struct rq *rq, struct task_struct *p)
 	schedtune_dequeue_task(p, task_cpu(p));
 	cpufreq_update_this_cpu(rq, SCHED_CPUFREQ_RT);
 }
+
+#ifdef CONFIG_SCHED_HMP
+static int
+select_task_rq_rt_hmp(struct task_struct *p, int cpu, int sd_flag, int flags)
+{
+	struct task_struct *curr, *tgt_task;
+	struct rq *rq;
+	bool may_not_preempt;
+	int target;
+
+	rq = cpu_rq(cpu);
+
+	rcu_read_lock();
+	curr = READ_ONCE(cpu_rq(cpu)->curr); /* unlocked access */
+
+	may_not_preempt = task_may_not_preempt(curr, cpu);
+	target = find_lowest_rq(p, 0);
+
+	/*
+	 * Check once for losing a race with the other core's irq handler.
+	 * This does not happen frequently, but it can avoid delaying
+	 * the execution of the RT task in those cases.
+	 */
+	if (target != -1) {
+		tgt_task = READ_ONCE(cpu_rq(target)->curr);
+		if (task_may_not_preempt(tgt_task, target))
+			target = find_lowest_rq(p, 0);
+	}
+
+	/*
+	* Possible race. Don't bother moving it if the
+	* destination CPU is not running a lower priority task.
+	*/
+	if (target != -1 && may_not_preempt)
+		cpu = target;
+	*per_cpu_ptr(&incoming_rt_task, cpu) = true;
+	rcu_read_unlock();
+
+	/*
+	 * If previous CPU was different, make sure to cancel any active
+	 * schedtune timers and deboost.
+	 */
+	if (task_cpu(p) != cpu) {
+		unsigned long fl;
+		struct rq *prq = task_rq(p);
+
+		raw_spin_lock_irqsave(&prq->lock, fl);
+		schedtune_dequeue_rt(prq, p);
+		raw_spin_unlock_irqrestore(&prq->lock, fl);
+	}
+
+	return cpu;
+}
+#endif
 
 static int
 select_task_rq_rt(struct task_struct *p, int cpu, int sd_flag, int flags,
